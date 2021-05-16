a6e21c73e5c1b14750c85fd9f06762dc4986a55c
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-05-16 12:47:00 +0300

    Final commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const timeContainer = document.querySelector(".timeContainer");
-const timerControlsContainer = document.querySelector(".timerControlsContainer");
+const timerControlsContainer = document.querySelector(
+  ".timerControlsContainer"
+);
 
 const timer = new Timer(timeContainer, timerControlsContainer);
